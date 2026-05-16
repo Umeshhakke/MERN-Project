34655299770710d5a5e409dac62c7c6e17514907
Umesh Hakke <umeshhakke050@gmail.com>
2026-05-17 01:16:20 +0530

    backend connection

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://192.168.137.1:5000/api' || 'http://localhost:5000/api',
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 // Attach token to every request if user is logged in
